814f55bb2d1cd2d045c8727f745c73e13755779d
bendorr2415 <70353777+bendorr2415@users.noreply.github.com>
2021-01-26 18:32:20 -0500

    got rid of '../' from paths

--- test/test_align.py
+++ test/test_align.py
@@ -1,8 +1,8 @@
 import pytest
 from align import algs
 
-fa1 = '../sequences/prot-0004.fa'
-fa2 = '../sequences/prot-0008.fa'
+fa1 = 'sequences/prot-0004.fa'
+fa2 = 'sequences/prot-0008.fa'
 scoringMatrixFile = 'BLOSUM50.mat'
 gapOpening = 11
 gapExtension = 3
